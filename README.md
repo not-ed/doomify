4535b1c9bce4d1174816687f7cdb518fc48ea99a
not-ed <43544402+not-ed@users.noreply.github.com>
2020-07-03 00:56:01 +0100

    First working version

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # doomify
-Image color indexing utility for creating DOOM textures
+Doomify is a utility for batch generating DOOM-style textures from images through color indexing. It is intended as a way to help automate the texture creation process for WAD makers and creatives looking to replicate the original art style of the game.
+
+<h1>Prerequisites</h1>
+- Python 3
+- Pillow (PIL library fork)
+
+```
+pip install Pillow
+```
---
+++ doomify.py
@@ -0,0 +1,64 @@
+from PIL import Image
+from pallete import DOOM
+import os
+
+def GetClosestColor(color):
+    closest_index = 0
+    closest_color_difference = 0
+
+    current_index = 0
+    for c in DOOM:
+        if current_index == 0:
+            closest_color_difference = GetTotalRGBDifference(color,c)
+            closest_index = 0
+        else:
+            current_color_difference = GetTotalRGBDifference(color,c)
+            if current_color_difference < closest_color_difference:
+                closest_color_difference = current_color_difference
+                closest_index = current_index
+        current_index += 1
+    return DOOM[closest_index]
+
+def GetTotalRGBDifference(color1,color2):
+    difference = 0
+    difference += abs(color1[0] - color2[0])
+    difference += abs(color1[1] - color2[1])
+    difference += abs(color1[2] - color2[2])
+    return difference
+
+#Start of program
+source_paths = input("Enter or drag image file path(s): ")
+source_paths = source_paths.split()
+if len(source_paths) == 0:
+    exit
+for path in source_paths:
+    source_image = Image.open(path)
+    source_image = source_image.resize((64,64),Image.NEAREST)
+    #This is needed for image formats that use a color table/palette.
+    source_image = source_image.convert("RGBA")
+    result = Image.new("RGB",source_image.size,color=(0,255,255))
+
+    print("Processing {} ...".format(path))
+    for i in range(source_image.size[0]):
+        for ii in range(source_image.size[1]):
+            source = source_image.getpixel((i,ii))
+            source_r,source_g,source_b = source[0],source[1],source[2]
+            if len(source) > 3: #alpha channel exists
+                if source[3] < 128:
+                    closest_color = (0,255,255)
+                else:
+                    closest_color = GetClosestColor((source_r,source_g,source_b))
+            else:
+                closest_color = GetClosestColor((source_r,source_g,source_b))
+            result.putpixel((i,ii),closest_color)
+    print("DONE!")
+
+    source_image.close()
+
+    output_path = os.path.join(os.getcwd(),"output")
+    if not os.path.isdir(output_path):
+        os.mkdir(output_path)
+
+    output_name = os.path.basename(path)
+    output_name = output_name.replace(".","_")
+    result.save(os.path.join(output_path,"doomify_{}.bmp".format(output_name)),format="bmp")
