f2fd6c831a6f9cac0ebdba047c7a9de0e84a0cf6
not-ed <43544402+not-ed@users.noreply.github.com>
2020-07-21 02:45:52 +0100

    Added color palette selection per-image and QUAKE color palette

--- doomify.py
+++ doomify.py
@@ -1,13 +1,13 @@
 from PIL import Image
-from pallete import DOOM
+from pallete import *
 import os
 
-def GetClosestColor(color):
+def GetClosestColor(color,palette):
     closest_index = 0
     closest_color_difference = 0
 
     current_index = 0
-    for c in DOOM:
+    for c in palette:
         if current_index == 0:
             closest_color_difference = GetTotalRGBDifference(color,c)
             closest_index = 0
@@ -17,7 +17,7 @@ def GetClosestColor(color):
                 closest_color_difference = current_color_difference
                 closest_index = current_index
         current_index += 1
-    return DOOM[closest_index]
+    return palette[closest_index]
 
 def GetTotalRGBDifference(color1,color2):
     difference = 0
@@ -26,13 +26,30 @@ def GetTotalRGBDifference(color1,color2):
     difference += abs(color1[2] - color2[2])
     return difference
 
+def GetDictionaryValueFromUser(prompt,dictionary):
+    print(prompt)
+    for k in dictionary:
+        print("{} : {}".format(k,dictionary[k][0]))
+    selection_is_valid = False
+    selection = ""
+    while not selection_is_valid:
+        selection = input()
+        try:
+            selection = dictionary[selection][1]
+            selection_is_valid = True
+        except KeyError:
+            print("Please enter a listed selection.")
+    return selection
+
 #Start of program
+palettes = {"1":("DOOM",DOOM),"2":("QUAKE",QUAKE)}
 source_paths = input("Enter or drag image file path(s): ")
 source_paths = source_paths.split()
 if len(source_paths) == 0:
     exit
 for path in source_paths:
     source_image = Image.open(path)
+    selected_palette = GetDictionaryValueFromUser("[{}]\n Please select a color paltte:".format(path),palettes)
     source_image = source_image.resize((64,64),Image.NEAREST)
     #This is needed for image formats that use a color table/palette.
     source_image = source_image.convert("RGBA")
@@ -47,9 +64,9 @@ for path in source_paths:
                 if source[3] < 128:
                     closest_color = (0,255,255)
                 else:
-                    closest_color = GetClosestColor((source_r,source_g,source_b))
+                    closest_color = GetClosestColor((source_r,source_g,source_b),selected_palette)
             else:
-                closest_color = GetClosestColor((source_r,source_g,source_b))
+                closest_color = GetClosestColor((source_r,source_g,source_b),selected_palette)
             result.putpixel((i,ii),closest_color)
     print("DONE!")
 
