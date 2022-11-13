e3f9046918dce2673da22d721c02775538445cba
not-ed <43544402+not-ed@users.noreply.github.com>
2022-11-13 19:36:17 +0000

    Adjusted GplPaletteLoader logic and added parsing outcomes
    A majority of GplPaletteLoader code has been adjusted to make it easier to reason about and has had assignments to `fileLoadOutcome` added for deviations from the happy path of execution.

--- src/palette_loaders/gplpaletteloader.cpp
+++ src/palette_loaders/gplpaletteloader.cpp
@@ -1,36 +1,71 @@
 #include "gplpaletteloader.h"
-
+#include <iostream>
 Palette GplPaletteLoader::LoadFile(QString file_path) {
+    fileLoadOutcome = UNKNOWN;
     Palette loaded_palette;
 
     std::ifstream opened_file;
     opened_file.open(file_path.toStdString(), std::ios::in);
 
-    while (!opened_file.eof()){
-        int c_red = 0;
-        int c_green = 0;
-        int c_blue = 0;
+    std::string read_name;
 
-        opened_file >> c_red;
-        opened_file >> c_green;
-        opened_file >> c_blue;
+    if (opened_file.is_open()){
+        std::string read_line;
+        std::getline(opened_file,read_line);
 
-        // If we invoked a read error by reading a non-int, skip past this line and continue.
-        if (opened_file.fail()){
-            opened_file.clear();
-            opened_file.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-            continue;
+        bool has_gimp_palette_indicator = QString(read_line.c_str()).trimmed().toLower() != "gimp palette";
+        if (has_gimp_palette_indicator){
+            fileLoadOutcome = FAILURE;
+            return loaded_palette;
         }
 
-        opened_file.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+        while (!opened_file.eof()){
+            std::getline(opened_file,read_line);
 
-        loaded_palette.colors.insert(loaded_palette.colors.begin(), QColor(c_red, c_green, c_blue));
+            bool line_is_comment = QString(read_line.c_str()).trimmed().startsWith("#");
+            if (line_is_comment){
+                continue;
+            }
+
+            if (read_name.empty()){
+                bool line_is_name = QString(read_line.c_str()).trimmed().toLower().startsWith("name:");
+                if (line_is_name){
+                    read_name = QString(read_line.c_str()).remove("name:",Qt::CaseInsensitive).trimmed().toStdString();
+                    continue;
+                }
+            }
+
+            QStringList split_line = QString(read_line.c_str()).simplified().split(" ");
+            if(split_line.length() >= 3){
+                bool red_value_is_valid;
+                int red_value = split_line[0].toInt(&red_value_is_valid);
+                red_value_is_valid = red_value_is_valid && (0 <= red_value && red_value <= 255);
+                bool green_value_is_valid;
+                int green_value = split_line[1].toInt(&green_value_is_valid);
+                green_value_is_valid = green_value_is_valid && (0 <= green_value && green_value <= 255);
+                bool blue_value_is_valid;
+                int blue_value = split_line[2].toInt(&blue_value_is_valid);
+                blue_value_is_valid = blue_value_is_valid && (0 <= blue_value && blue_value <= 255);
+
+                if (red_value_is_valid && green_value_is_valid && blue_value_is_valid){
+                    loaded_palette.colors.insert(loaded_palette.colors.begin(), QColor(red_value, green_value, blue_value));
+                }
+                else if (!loaded_palette.colors.empty()) {
+                    fileLoadOutcome = MISSING_COLORS;
+                }
+            }
+        }
+    }
+    else
+    {
+        fileLoadOutcome = FAILURE;
+        return loaded_palette;
     }
 
     opened_file.close();
 
-    loaded_palette.displayName = QFileInfo(file_path).fileName();
+    loaded_palette.displayName = read_name.empty() ? QFileInfo(file_path).fileName() : QString(read_name.c_str());
 
-    fileLoadOutcome = SUCCESSFUL;
+    if (fileLoadOutcome == UNKNOWN) fileLoadOutcome = SUCCESSFUL;
     return loaded_palette;
 }
